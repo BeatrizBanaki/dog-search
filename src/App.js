e9d99e53b64e5db664cf6fec5e32a655ca1a8e11
Beatriz Banaki <112290703+BeatrizBanaki@users.noreply.github.com>
2024-11-15 14:23:52 -0300

    ajusta mensagem de erro

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import React from 'react';
-import { Container, Typography, Alert } from '@mui/material';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Container, Typography, Button, Alert } from '@mui/material';
 import SearchForm from './components/SearchForm';
 import DogList from './components/DogList';
+import BreedsList from './components/BreedsList';
+import RandomDogImage from './components/RandomDogImage';
 import { DogProvider, useDogContext } from './context/DogContext';
 
 const App = () => {
   return (
     <DogProvider>
-      <Container style={{ padding: '2rem' }}>
-        <Typography variant="h4" gutterBottom>Dog Breed Search</Typography>
-        <DogContent />
-      </Container>
+      <Router>
+        <Container style={{ padding: '2rem' }}>
+          <Typography variant="h4" gutterBottom>
+            Dog Breed Search
+          </Typography>
+          <nav style={{ marginBottom: '1rem' }}>
+            <Button
+              component={Link}
+              to="/"
+              variant="contained"
+              color="primary"
+              style={{ marginRight: '1rem' }}
+            >
+              Home
+            </Button>
+            <Button
+              component={Link}
+              to="/breeds"
+              variant="contained"
+              color="secondary"
+              style={{ marginRight: '1rem' }}
+            >
+              All Breeds
+            </Button>
+            <Button
+              component={Link}
+              to="/random"
+              variant="contained"
+              color="success"
+            >
+              Random Dog Image
+            </Button>
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<DogContent />} />
+            <Route path="/breeds" element={<BreedsList />} />
+            <Route path="/random" element={<RandomDogImage />} />
+          </Routes>
+        </Container>
+      </Router>
     </DogProvider>
   );
 };
@@ -20,7 +60,7 @@ const DogContent = () => {
 
   return (
     <>
-      {errorMessage && <Alert severity="error">{errorMessage}</Alert>} {/* Exibir mensagens de erro */}
+      {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
       <SearchForm onSearch={fetchImages} />
       <DogList images={images} />
     </>

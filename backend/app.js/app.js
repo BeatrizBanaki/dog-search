2ed865a70515aa2487ea93907a33980f3cb29e76
Beatriz Banaki <112290703+BeatrizBanaki@users.noreply.github.com>
2025-02-07 16:58:21 -0300

    Ajusta App

--- backend/app.js/app.js
+++ backend/app.js/app.js
@@ -2,12 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
+const breedRoutes = require('./routes/breedRoutes'); // Importe as rotas de raças
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
+// Rotas públicas
 app.use('/api/auth', authRoutes);
+
+// Rotas protegidas (requerem autenticação)
+app.use('/api/breeds', breedRoutes);
 
 module.exports = app;

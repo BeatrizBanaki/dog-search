bc3605c5bc22585e1e79fd5077efb7d495ae194b
Beatriz Banaki <112290703+BeatrizBanaki@users.noreply.github.com>
2025-02-09 15:03:27 -0300

    Ajustes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,16 +1,18 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const logger = require('../utils/logger');
 
 const login = async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findByUsername(username);
 
   if (!user || !(await bcrypt.compare(password, user.password))) {
+    logger.error(`Falha de autenticação para o usuário: ${username}`); // Registra o erro
     return res.status(401).json({ message: 'Invalid credentials' });
   }
 
-  const token = jwt.sign({ id: user.id }, 'secret_key');
+  const token = jwt.sign({ id: user.id }, 'secret_key', { expiresIn: '1h' });
   res.json({ token });
 };
 
